0df4b1c49a62b48088d1e362b64d56fcc837e273
Aaryan Pashine <pashineaaryan@gmail.com>
2023-02-07 14:26:38 -0500

    trinket addition

---
+++ trinket/data.js
@@ -0,0 +1,271 @@
+let data = [
+  {
+    name: "Sterling Wedge",
+    title: "frog",
+    description: "a small metal frog.",
+    tag1: "animal",
+    tag2: "metal",
+    tag3: "gift",
+    image: "images/frog.jpg",
+  },
+  {
+    name: "Yuki",
+    title: "light",
+    description: "a pink sea urchin night light",
+    tag1: "pink",
+    tag2: "plastic",
+    tag3: "animal",
+    image: "images/light.jpg",
+  },
+  {
+    name: "Thao",
+    title: "rabbit",
+    description: "a small white ceramic chopstick holder",
+    tag1: "cute",
+    tag2: "animal",
+    tag3: "ceramic",
+    image: "images/thao.jpg",
+  },
+  {
+    name: "Omama",
+    title: "heart-earring",
+    description:
+      "A pink heart-shaped earring that was given to me by one of my best friends for my 20th birthday.",
+    tag1: "Pink",
+    tag2: "gem",
+    tag3: "heart",
+    image: "images/heart earrings.jpg",
+  },
+  {
+    name: "Michelle",
+    title: "Earring",
+    description: "a metal jewelery",
+    tag1: "aesthetic",
+    tag2: "stylish",
+    tag3: "handy",
+    image: "images/Earring.jpeg",
+  },
+  {
+    name: "Aaryan",
+    title: "sharpener",
+    description: "stolen silver sharpener",
+    tag1: "stationary",
+    tag2: "gift",
+    tag3: "metal",
+    image: "images/sharpener.jpeg",
+  },
+  {
+    name: "Sydney",
+    title: "keychain",
+    description: "black and white knotted rope ckeychain with metal end caps.",
+    tag1: "black",
+    tag2: "rope",
+    tag3: "keychain",
+    image: "images/keychiain.jpeg",
+  },
+  {
+    name: "Dima Lavrentiev",
+    title: "Mushroom necklace",
+    description: "a pendant made out of moonstone with a silver chain",
+    tag1: "jewelry",
+    tag2: "stone",
+    tag3: "plant",
+    image: "images/Mushroom_necklace.jpg",
+  },
+  {
+    name: "Bella Wu",
+    title: "Breastpin",
+    description: "a metallic blue jewelry",
+    tag1: "Blue",
+    tag2: "gift",
+    tag3: "twinkle",
+    image: "images/Breastpin.jpg",
+  },
+  {
+    name: "Rylee Tan",
+    title: "Toy",
+    description: "a couple of sailboats inside of a glass bottle shape",
+    tag1: "Ocean",
+    tag2: "summer",
+    tag3: "Fish",
+    image: "images/Bottle Boat.JPG",
+  },
+  {
+    name: "Yilin Li",
+    title: "Bracelet ",
+    description: "A hand-woven bracelet with a gold-plated ornaments",
+    tag1: "traditional",
+    tag2: "handmade",
+    tag3: "",
+    image: "images/bracelet.jpg",
+  },
+  {
+    name: "Kelly Chen",
+    title: "Heart Headphone Splitter",
+    description: "A pink heart-shaped, two-way headphone splitter.",
+    tag1: "Pink",
+    tag2: "Electronic",
+    tag3: "Keychain",
+    image: "images/heart-connector.JPG",
+  },
+  {
+    name: "Ian Luib",
+    title: "Star-Keychain",
+    description: "A metallic keychain shaped like a star with pink accents",
+    tag1: "Pink",
+    tag2: "Metallic",
+    tag3: "Shiny",
+    image: "images/noPhoto.png",
+  },
+  {
+    name: "Samantha Liu",
+    title: "Flower bracelet",
+    description:
+      "woven bracelet made from blue, yellow and white strings that forms a flower pattern.",
+    tag1: "Gift",
+    tag2: "Flower",
+    tag3: "Accessory",
+    image: "images/flower.JPG",
+  },
+  {
+    name: "Jose Montero",
+    title: "Pokemon",
+    description: "A small, plastic figure of the pokemon Garchomp",
+    tag1: "animal",
+    tag2: "blue",
+    tag3: "plastic",
+    image: "images/pokemon.jpg",
+  },
+  {
+    name: "Amani Mohamed",
+    title: "film case",
+    description: "the outter shell of a film roll",
+    tag1: "metal",
+    tag2: "orange/black",
+    tag3: "Analog ",
+    image: "images/filmroll.jpg",
+  },
+  {
+    name: "Uzoma Ekpunobi",
+    title: "goldface earring",
+    description: "earring with goldface on it",
+    tag1: "metal",
+    tag2: "jewelry",
+    tag3: "face",
+    image: "images/goldface_earrings.jpg",
+  },
+  {
+    name: "Finnbogi Örn",
+    title: "handsome dog",
+    description: "a small enamel pin, subject is a dog with a human face",
+    tag1: "metal",
+    tag2: "animal",
+    tag3: "stylish",
+    image: "images/handsome-dog.jpg",
+  },
+  {
+    name: "Sadie Lambert",
+    title: "eye ring",
+    description: "silver ring with a blue glass eyeball on it",
+    tag1: "jewelery",
+    tag2: "eye",
+    tag3: "blue",
+    image: "images/eyeball-ring.jpg",
+  },
+  {
+    name: "Adarsh Mani",
+    title: "Wooden Face Keychain",
+    description:
+      "A wooden carved keychain of a facial expression with a metal chain extension",
+    tag1: "Wood",
+    tag2: "Face",
+    tag3: "Keychain",
+    image: "images/woodenfacekeychain.jpg",
+  },
+  {
+    name: "Delusha Wanigasuriya",
+    title: "Airpods",
+    description: "A small white case with bluetooth earphones inside",
+    tag1: "electronic",
+    tag2: "music",
+    tag3: "",
+    image: "images/Airpods.JPG",
+  },
+  {
+    name: "Sammi Lam",
+    title: "Crystal Bear",
+    description: "A clear, bear-shaped keychain.",
+    tag1: "animal",
+    tag2: "keychain",
+    tag3: "clear",
+    image: "images/IMG_4300.jpg",
+  },
+  {
+    name: "Anosha Khalid",
+    title: "Cooking Mama game",
+    description: "Nintendo DS lite game chip ",
+    tag1: "video game",
+    tag2: "nostalgic",
+    tag3: "",
+    image: "images/cookingmama.jpg",
+  },
+  {
+    name: "Kirthiga ",
+    title: "lip balm",
+    description: "a yellow and red lip balm",
+    tag1: "",
+    tag2: "",
+    tag3: "",
+    image: "images/noPhoto.png",
+  },
+  {
+    name: "Esther Sales",
+    title: "",
+    description: "Friendship ring with Omama",
+    tag1: "",
+    tag2: "",
+    tag3: "",
+    image: "images/friendship-ring.jpg",
+  },
+  {
+    name: "Kelly La",
+    title: "Polaroid",
+    description:
+      "A polariod photo of my friends hanging out at Trinity Bellwoods",
+    tag1: "Spring",
+    tag2: "Friends",
+    tag3: "Picnic",
+    image: "images/kellylporloaid.JPG",
+  },
+];
+
+let fullCode = "";
+for (const x of data) {
+  code = `<div class="${x.name} trinket" >`;
+  let h1 = `<div class="heading"><h1>${x.title}</h1></div>`;
+  let img = `<img class="trinket-image" src="${x.image ? x.image : ""}" />`;
+  let h3 = `<div class="subtitle"><h3>${x.name}</h3></div>`;
+  let p = `<div class="description"><p>${x.description}</p></div>`;
+  code += img + h1 + h3 + p;
+  code += "</div>";
+  fullCode += code;
+}
+
+document.querySelector(".container").innerHTML += fullCode;
+
+// Use are.na to do this
+
+// get text blocks from a channel
+let channel = new Arena("trinket-css/");
+channel.texts().then((res) => {
+  for (const x of res) addStyle(x.content);
+});
+
+// for each text box run a add styles where arena content is the style
+function addStyle(str) {
+  let styleSheet = document.createElement("style");
+  styleSheet.innerText = str;
+  document.head.appendChild(styleSheet);
+}
+
+console.log("no photo found");
